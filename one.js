a99068c86519afc15d7f922317823bc660c67ca1
vasudev290 <vasukesavulu@gmail.com>
2024-03-20 17:05:49 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,20 @@
+//objects...
+let person = {
+    name: "Vasu dev",
+    age: 23,
+    Isalive: true,
+    gender: "male",
+    address: "Bangaluru, karnathaka, india",
+    sibling: {
+        brother: "Kiran",
+        sister:"Sushmitha",    
+    }
+
+}
+person.age = 25
+console.log(person.name)
+console.log(person.age)
+console.log(person.Isalive)
+console.log(person.gender)
+console.log(person.address)
+console.log(person.sibling)
